f3a9b3625b424dca53fe27d139a448f25e5fe4b1
Ludvig Kalberg <kalbergludvig@outlook.com>
2026-04-13 14:32:44 +0200

    Add Port result properties

---
+++ NetGuard.Core/PortResult.cs
@@ -0,0 +1,11 @@
+/*
+needs
+a number
+open or closed
+*/
+namespace NetGuard.Core{
+    public class PortResult {
+        public int PortNumber { get; set; }
+        public bool IsOpen { get; set; }
+    }
+}

33d3769a74e88c9937c482b3f2572b2fc65ef6a3
Ludvig Kalberg <kalbergludvig@outlook.com>
2026-04-14 14:20:11 +0200

    Add scanner service has empty Scan method

---
+++ NetGuard.Core/ScannerService.cs
@@ -0,0 +1,12 @@
+/*
+takes in target
+outputs scan result
+*/
+
+namespace NetGuard.Core{
+    public class ScannerService{
+        public ScanResult Scan(string hostname){
+            return new ScanResult();
+        }
+    }
+}
